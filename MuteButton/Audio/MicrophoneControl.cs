c23ebcff0342a87c81b2e23b84b410a0a007311c
haug1 <larsiah@hotmail.com>
2023-12-13 20:42:32 +0100

    .net 8.0 + visual studio migrate

---
+++ MuteButton/Audio/MicrophoneControl.cs
@@ -0,0 +1,81 @@
+﻿using NAudio.CoreAudioApi;
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+namespace MuteButton.Audio {
+
+  public delegate void OnMicrophoneToggledEvent(MMDevice microphone, bool isMuted);
+
+  class MicrophoneControl : IDisposable {
+    private MMDevice? _microphone = null;
+    private bool? _lastMutedStatus = null;
+    private CancellationTokenSource? _soundCancellation = null;
+
+    public event OnMicrophoneToggledEvent? OnMicrophoneToggled = null;
+
+    public bool? IsMuted {
+      get {
+        return _microphone?.AudioEndpointVolume.Mute;
+      }
+    }
+
+    public string? SelectedDevice {
+      get {
+        return _microphone?.DeviceFriendlyName;
+      }
+    }
+
+    public MicrophoneControl() {
+      try {
+        var defaultDevice = ListFriendlyDeviceNames().Last();
+        SetDevice(defaultDevice);
+      } catch {
+        Debug.WriteLine("WARN: Failed to set microphone on startup.");
+      }
+    }
+
+    public static IEnumerable<string> ListFriendlyDeviceNames() {
+      return _enumerateMicrophoneDevices().Select(device => device.DeviceFriendlyName).ToImmutableList();
+    }
+
+    public void SetDevice(string deviceFriendlyName) {
+      Dispose();
+      _microphone = _enumerateMicrophoneDevices().First(device => device.DeviceFriendlyName == deviceFriendlyName);
+      _microphone.AudioEndpointVolume.OnVolumeNotification += _onVolumeNotification;
+    }
+
+    public void ToggleMicrophone(bool? forceMute = null) {
+      if (_microphone != null)
+        _microphone.AudioEndpointVolume.Mute = forceMute.HasValue ? forceMute.Value : !_microphone.AudioEndpointVolume.Mute;
+    }
+
+    public void Dispose() {
+      if (_microphone != null) {
+        ToggleMicrophone(false);
+        _microphone.AudioEndpointVolume.OnVolumeNotification -= _onVolumeNotification;
+        _microphone.Dispose();
+      }
+    }
+
+    private static MMDeviceCollection _enumerateMicrophoneDevices() {
+      using MMDeviceEnumerator enumerator = new();
+      return enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+    }
+
+    void _onVolumeNotification(AudioVolumeNotificationData data) {
+      if (_lastMutedStatus != data.Muted) {
+        _lastMutedStatus = data.Muted;
+        Debug.WriteLine($"Microphone toggled: IsMuted={data.Muted}");
+        OnMicrophoneToggled?.Invoke(_microphone!, data.Muted);
+        _soundCancellation?.Cancel();
+        _soundCancellation = new CancellationTokenSource();
+
+        var micStatus = _microphone!.AudioEndpointVolume.Mute ? Properties.Resources.microphone_muted_teamspeak : Properties.Resources.microphone_activated_teamspeak;
+        _ = SoundPlayer.PlaySound(
+           micStatus,
+          _soundCancellation.Token
+        );
+      }
+    }
+  }
+}
---
+++ MuteButton/Audio/SoundPlayer.cs
@@ -0,0 +1,30 @@
+﻿using NAudio.Wave;
+
+namespace MuteButton.Audio {
+  public static class SoundPlayer {
+    public static async Task PlaySound(byte[] bytes, CancellationToken cancellationToken) {
+      using var memStream = new MemoryStream(bytes);
+      using var mp3 = new Mp3FileReader(memStream);
+      using var waveOut = new WaveOutEvent();
+      waveOut.Init(mp3);
+      waveOut.Volume = 0.05f;
+      waveOut.Play();
+      var tcs = new TaskCompletionSource<object>();
+      _ = Task.Run(async () => {
+        while (waveOut.PlaybackState == PlaybackState.Playing) {
+          await Task.Delay(100);
+
+          if (cancellationToken.IsCancellationRequested) {
+            waveOut.Stop();
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            tcs.TrySetResult(null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            return;
+          }
+        }
+      }, cancellationToken);
+
+      await tcs.Task;
+    }
+  }
+}
